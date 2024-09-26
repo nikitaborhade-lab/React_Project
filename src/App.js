710d81b3a3f3a1320a341cddf185e71fc03c41f0
nikitaborhade-lab <niki.s.borhade@gmail.com>
2024-09-26 18:31:57 +0530

    added packages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from 'react'
 import './App.css';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
+      <SearchBar />
     </div>
   );
 }
